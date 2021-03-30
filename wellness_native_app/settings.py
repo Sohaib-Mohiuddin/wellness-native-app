a69ce210863b72caf8442402bf8516e91292ea5b
Sohaib-Windows <sohaib.mohiuddin@uoit.net>
2021-03-30 19:09:12 -0400

    Email service complete

--- wellness_native_app/settings.py
+++ wellness_native_app/settings.py
@@ -165,3 +165,11 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Email Backend Service
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('WELLNESS_EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('WELLNESS_EMAIL_PASS')
