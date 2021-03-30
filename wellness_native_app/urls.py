8f38a8665c8f84b936d631a6363c7ac650fca410
Sohaib-Windows <sohaib.mohiuddin@uoit.net>
2021-03-30 19:01:17 -0400

    Partial password reset and functions

--- wellness_native_app/urls.py
+++ wellness_native_app/urls.py
@@ -32,6 +32,9 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('register/', users_views.register, name='register'),
     path('profile/', users_views.profile, name='profile'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
 
     # food_app views
     path('', food_app_views.home, name='food-app-home'),
